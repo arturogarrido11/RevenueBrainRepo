06740dfeefef2044a63c2a5e8785e2176d652a08
Arturogarrido11 <arturo.garrido11@gmail.com>
2026-09-07 16:47:44 -0400

    fix(convex): remove unused uuid import causing module load failure in Convex runtime

--- convex/http.ts
+++ convex/http.ts
@@ -1,7 +1,6 @@
 import { httpRouter } from "convex/server"
 import { httpAction } from "./_generated/server"
 import { internal } from "./_generated/api"
-import { v4 as uuidv4 } from "uuid"
 
 const http = httpRouter()
 
